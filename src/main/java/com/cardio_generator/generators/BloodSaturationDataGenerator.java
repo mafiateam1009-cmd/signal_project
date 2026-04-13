ff737c4b7800e65b483e915a763d1112b431b351
Vlad Cretu <v.cretu@student.maastrichtuniversity.nl>
2026-04-13 19:56:00 +0200

    added doc in BloodSaturationDataGenerator

--- src/main/java/com/cardio_generator/generators/BloodSaturationDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/BloodSaturationDataGenerator.java
@@ -4,10 +4,19 @@ import java.util.Random;
 
 import com.cardio_generator.outputs.OutputStrategy;
 
+/**
+ * This class generates blood saturation (SpO2) data for patients.
+ * It keeps track of the last value so the changes look more realistic over time.
+ */
 public class BloodSaturationDataGenerator implements PatientDataGenerator {
     private static final Random random = new Random();
     private int[] lastSaturationValues;
 
+    /**
+     * Constructor that sets up the starting saturation for all patients.
+     * Each patient starts with a healthy baseline between 95% and 100%.
+     *  @param patientCount the number of patients to create data for
+     */
     public BloodSaturationDataGenerator(int patientCount) {
         lastSaturationValues = new int[patientCount + 1];
 
@@ -17,6 +26,12 @@ public class BloodSaturationDataGenerator implements PatientDataGenerator {
         }
     }
 
+    /**
+     * Generates a new saturation value by adding a small random change to the last one.
+     * It also makes sure the value stays in a safe range (90-100%).
+     * * @param patientId the ID of the patient we are generating for
+     * @param outputStrategy where to send the generated data
+     */
     @Override
     public void generate(int patientId, OutputStrategy outputStrategy) {
         try {
@@ -30,6 +45,7 @@ public class BloodSaturationDataGenerator implements PatientDataGenerator {
             outputStrategy.output(patientId, System.currentTimeMillis(), "Saturation",
                     Double.toString(newSaturationValue) + "%");
         } catch (Exception e) {
+            // Log the error if something goes wrong with the generation
             System.err.println("An error occurred while generating blood saturation data for patient " + patientId);
             e.printStackTrace(); // This will print the stack trace to help identify where the error occurred.
         }
