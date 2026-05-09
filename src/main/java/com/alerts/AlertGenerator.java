75bd53b31fc56611aa8f837904a3c62021132be0
DavidNgounou <ngounoudavid99@gmail.com>
2026-05-09 16:25:49 +0200

    made the check blood pressure... method to have public visibility

--- src/main/java/com/alerts/AlertGenerator.java
+++ src/main/java/com/alerts/AlertGenerator.java
@@ -99,7 +99,7 @@ public class AlertGenerator {
      * @param changeThreshold the minimum average change in blood pressure values over the specified window size that would indicate an increasing trend and trigger an alert
      * @param windowSize the number of consecutive records to consider when evaluating the trend
      */
-    void checkBloodPressureIncreasingTrend(Patient patient, double changeThreshold, int windowSize) {
+    public void checkBloodPressureIncreasingTrend(Patient patient, double changeThreshold, int windowSize) {
         List<PatientRecord> records = patient.getRecordsbyType("BloodPressure");
 
         for(int i = 0; i < records.size() - windowSize; i++) {
